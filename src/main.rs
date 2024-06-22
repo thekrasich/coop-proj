80483742d30672d0b4dd815cdaf54d6592124b36
thekrasich <91842995+thekrasich@users.noreply.github.com>
2024-06-22 15:33:34 +0300

    basic window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use eframe::egui;
+
 fn main() {
-    println!("Hello, world!");
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "My App",
+        options,
+        Box::new(|_cc| Box::new(MyApp::default())),
+    );
+}
+
+#[derive(Default)]
+struct MyApp;
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello, World!");
+        });
+    }
 }
